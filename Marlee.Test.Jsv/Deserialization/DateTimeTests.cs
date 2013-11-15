5ed5de5ffabc387ee9ec253364a082f27bc6bb5b
Julian <julian.rooze@gmail.com>
2013-11-15 19:48:05 +0100

    Test stub

---
+++ Marlee.Test.Jsv/Deserialization/DateTimeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceStack.Text;
+using Marlee.Jsv;
+
+namespace Marlee.Test.Jsv.Deserialization
+{
+  [TestClass]
+  public class DateTimeTests
+  {
+    public class DateTimeProperty
+    {
+      public DateTime Date { get; set; }
+    }
+    [TestMethod]
+    public void Test_1()
+    {
+      var instance = new DateTimeProperty
+      {
+        Date = new DateTime(2013, 10, 1, 14, 45, 28, DateTimeKind.Local)
+      };
+
+      var serialized = TypeSerializer.SerializeToString(instance);
+
+      var deserializer = new JsvConverter();
+
+      var result = deserializer.DeserializeFromString<DateTimeProperty>(serialized);
+    }
+  }
+}
