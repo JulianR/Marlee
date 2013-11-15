21ab0cd346227349dee098d4ed6379680149ca99
Julian <julian.rooze@gmail.com>
2013-11-15 19:49:47 +0100

    removed dependency on dictionary and instead now use a trick based on .NET generics to get the right deserializer

---
+++ Marlee/Jsv/Deserialization/DeserializerStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Marlee.Internal;
+
+namespace Marlee.Jsv.Deserialization
+{
+  public class DeserializerStore<T>
+  {
+    public static DeserializeHandler<T> Function;
+  }
+}
--- Marlee/Jsv/JsvConverter.cs
+++ Marlee/Jsv/JsvConverter.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Marlee.Common.Deserialization;
 using Marlee.Common;
 using Marlee.Internal;
+using Marlee.Jsv.Deserialization;
 
 namespace Marlee.Jsv
 {
@@ -25,18 +26,28 @@ namespace Marlee.Jsv
 
     public T DeserializeFromString<T>(string data)
     {
-      Deserializer deserializer;
-
-      if (!_deserializerCache.TryGetValue(typeof(T), out deserializer))
+      var func = DeserializerStore<T>.Function;
+      if (func == null)
       {
-        deserializer = _factory.CreateDeserializer(typeof(T));
-
-        _deserializerCache.Add(typeof(T), deserializer);
+        var deserializer = _factory.CreateDeserializer(typeof(T));
+        func = DeserializerStore<T>.Function = (DeserializeHandler<T>)deserializer.Method;
       }
 
       int start = 0;
+      return func(ref start, data);
 
-      return ((DeserializeHandler<T>)deserializer.Method)(ref start, data);
+      //Deserializer deserializer;
+
+      //if (!_deserializerCache.TryGetValue(typeof(T), out deserializer))
+      //{
+      //  deserializer = _factory.CreateDeserializer(typeof(T));
+
+      //  _deserializerCache.Add(typeof(T), deserializer);
+      //}
+
+      //int start = 0;
+
+      //return ((DeserializeHandler<T>)deserializer.Method)(ref start, data);
     }
   }
 }
